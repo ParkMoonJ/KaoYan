99c3c9eb6ec70af16d93d638682a92a4d1db932d
ParkMoonJ <parkmoonj.9911@gmail.com>
2021-04-09 14:40:34 +0800

    Update queue.cpp

--- 3 栈和队列/queue.cpp
+++ 3 栈和队列/queue.cpp
@@ -12,6 +12,60 @@ void InitQueue(Queue &Q) {
     Q.rear = 0;
 }
 
+void ClearQueue(Queue &Q) {
+    while (Q.rear > Q.front) {
+        Q.data[Q.rear] = NULL;
+    }
+}
+
+bool QueueEmpty(Queue Q) {
+    return Q.rear == Q.front;
+}
+
+int QueueLength(Queue Q) {
+    return Q.rear;
+}
+
+bool GetHead(Queue Q, ElemType &e) {
+    if (Q.rear == Q.front) {
+        return false;
+    } else {
+        e = Q.data[Q.front];
+        Q.front++;
+        return true;
+    }
+}
+
+bool EnQueue(Queue &Q, ElemType e) {
+    if (Q.rear >= MAXSIZE - 1) {
+        return false;
+    } else {
+        Q.data[Q.rear] = e;
+        ++Q.rear;
+        return true;
+    }
+}
+
+bool DeQueue(Queue &Q, ElemType &e) {
+    if (Q.rear == Q.front) {
+        return false;
+    } else {
+        e = Q.data[Q.front];
+        ++Q.front;
+        return true;
+    }
+}
+
+bool QueueTraverse(Queue Q, visit()) {
+    if (Q.rear == Q.front) {
+        return false;
+    } else {
+        for (int i = Q.front; i < Q.rear; i++) {
+            visit(Q.data[i]);
+        }
+    }
+}
+
 int main() {
     return 0;
 }
